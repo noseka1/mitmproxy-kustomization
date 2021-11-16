3bedecaf6412ad084d404f85b300a8ce13f88e31
Ales Nosek <ales.nosek@gmail.com>
2021-11-16 13:21:37 -0800

    Create a separate run.sh script

---
+++ mitmproxy/base/conf/run.sh
@@ -0,0 +1,8 @@
+mitmdump \
+--set confdir=/home/mitmproxy \
+--set block_global=false \
+--set console_eventlog_verbosity=info \
+--set flow_detail=3 \
+--set ssl_insecure=true \
+--set stream_large_bodies=1 \
+--set termlog_verbosity=info
